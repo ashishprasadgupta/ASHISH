0775a9ce94a21f1a21d2ded34e976e9000efa03f
ashishprasadgupta <prasadguptaashish@gmail.com>
2022-10-11 14:01:48 +0530

    intialized

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Restaurant from './component/Basic/Restaurant';
+
+const App = () => {
+  return (
+    <div>
+      <Restaurant />
+          </div>
+  )
+}
+
+export default App;
